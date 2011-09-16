62ad4338335b6881ca768e73af5bd67035d06803
Nicco Kunzmann <niccokunzmann@googlemail.com>
2011-09-16 14:43:17 +0200

    added this connection that reconnects to a new connectio read from factory

---
+++ stream/FactoryConnection.py
@@ -0,0 +1,30 @@
+
+from Connection import *
+
+
+class UnableToConnectError(ConnectionError):
+    pass
+
+
+class FactoryConnection(Connection):
+
+    def __init__(self, factory):
+        '''create a new connection
+the factory as firt argument returns a new stream when calling read(1)'''
+        self.factory = factory
+        Connection.__init__(self)
+
+    def connect(self):
+        '''connect to the stream returned by the factory'''
+        if self.isConnected():
+            raise ConnectedError('the connection has to be disconnected'\
+                                 ' to connect again')
+        self.factory.update()
+        stream = self.factory.read(1)
+        if type(stream) is list:
+            if not stream:
+                raise UnableToConnectError('cannot connect because nothing was'\
+                                ' read from the factory %r' % self.factory)
+            stream = stream[0]
+        self.connectTo(stream)
+        
