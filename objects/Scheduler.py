e7ccc39df7896e36200552f87b1620ba1a75c2e7
Nicco Kunzmann <niccokunzmann@googlemail.com>
2011-09-25 20:00:38 +0200

    scheduler funzt

--- objects/Scheduler.py
+++ objects/Scheduler.py
@@ -1,23 +1,44 @@
 
 import threading
+import traceback
 import thread
 import heapq
 import Queue
 import time
-    
+
+_debugLock = thread.allocate_lock()
+_globali = 0
+_threadid = 0
 
 def debugMethod(func):
+    name = func.__name__
     def call(*args, **kw):
-        s = '%s%r, %s' % (func.__name__, args, kw)
-        print s
+        global _globali
+        try:
+            s = '%s%r, %r' % (name, args, kw)
+        except:
+            s = '%s (...)' % name
+        if 1:
+            i = _globali
+            _globali += 1
+            print '%i-> %s' % (i, s)
         r = func(*args, **kw)
-        print s, '=', r
+        s = '%i = %r' % (i, r)
+        if 1:
+            print s
         return r
+    call.__name__ = name
     return call
+
+##def debugMethod(func):
+##    return func
 
 class Scheduler(threading.Thread):
 
+    debug = 1
+
     maxWaitingThreads = 1
+
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -30,38 +51,51 @@ class Scheduler(threading.Thread):
         self.__running = 0
         self.__waiting = 0
 
+
     def stopped(self):
         return self.__stop
 
+
     def addJob(self, job, seconds):
         t = time.time() + seconds
+
         self._newJobs.put((job, t))
+
 
     def run(self):
         while not self.stopped():
             self.__addJobs(block = False)
             now = time.time()
-            print now
+
             while self.__jobs:
                 job, t = self.__getJob()
                 if t < now:
+
                     self.__doJob(job)
                 else:
+
                     self.__addJob(job, t)
+                    time.sleep(0.001)
+                    now = time.time()
             self.__addJobs(block = True) # last line in loop
+
 
     def __addJobs(self, block = True):
         while block or not self._newJobs.empty():
             newJob, time = self._newJobs.get()
             self.__addJob(newJob, time)
+            block = False
+
 
     def __addJob(self, job, time):
         heapq.heappush(self.__jobs, (time, job))
+
 
     def __getJob(self):
         time, job = heapq.heappop(self.__jobs)
         return job, time
             
+
     def stop(self):
         self.__stop = True
         with self._lock:
@@ -69,39 +103,60 @@ class Scheduler(threading.Thread):
                 self.__addJob(None, 0)
             self.addJob(None, 0)
 
+
     def __doJob(self, job):
         with self._lock:
-            self._doJobs.push(job)
+
+            self._doJobs.put(job)
             if not self._maxThreadsReached():
                 thread.start_new(self.runJob, ())
+
 
     def _maxThreadsReached(self):
         return self.__waiting >= self.maxWaitingThreads
 
-    def runJob(self):
-        job = self.__waitJob()
-        while job is not None and not self.stopped():
-            self.__runJob(job)
+
+    def runJob(self, *args, **kw):
+        try:
             job = self.__waitJob()
+
+            while job is not None and not self.stopped():
+
+                self.__runJob(job)
+
+                job = self.__waitJob()
+
+        except:
+            if self.debug >= 1:
+                traceback.print_exc()
             
+
     def __waitJob(self):
+        with self._lock:
+            if self._maxThreadsReached():
+                return None
+            self.__waiting += 1
+
+        try:
+            return self._doJobs.get()
+        finally:
             with self._lock:
-                if self._maxThreadsReached():
-                    return None
-                self.__waiting += 1
-            try:
-                return self._doJobs.pop()
-            finally:
-                with self._lock:
-                    self.__waiting -= 1
+
+                self.__waiting -= 1
     
+
     def __runJob(self, job):
         with self._lock:
+
             self.__running += 1
         try:
             job.run()
+        except:
+            if self.debug >= 1:
+                traceback.print_exc()
         finally:
             with self._lock:
+
                 self.__running -= 1
             
             
--- test/test_Scheduler.py
+++ test/test_Scheduler.py
@@ -4,8 +4,8 @@ from objects.Job import Job, Scheduler, defaultScheduler
 
 class CountJob(Job):
 
-    def __init__(self, count, timeout = 0.005):
-        Job.__init__(self)
+    def __init__(self, count, timeout = 0.005, *args):
+        Job.__init__(self, *args)
         self.count = count
         self.timeout = timeout
         
@@ -21,13 +21,13 @@ class test_Job(unittest.TestCase):
     def test_count_1(self):
         c = CountJob(1)
         c.run()
-        time.sleep(1)
+        time.sleep(0.8)
         self.assertEqual(0, c.count)
 
     def test_count_3(self):
         c = CountJob(3)
         c.run()
-        time.sleep(c.timeout * (c.count + 1))
+        time.sleep(0.8)
         self.assertEqual(0, c.count)
 
 class test_Scheduler(unittest.TestCase):
@@ -38,13 +38,13 @@ class test_Scheduler(unittest.TestCase):
     def test_addJob_1(self):
         job = CountJob(1)
         job.after(0)
-        time.sleep(0.01)
+        time.sleep(0.5)
         self.assertEqual(0, job.count)
 
     def test_addJob(self):
         job = CountJob(1)
         defaultScheduler.addJob(job, 0)
-        time.sleep(0.01)
+        time.sleep(0.5)
         self.assertEqual(0, job.count)
 
     def test_create_add(self):
@@ -52,14 +52,16 @@ class test_Scheduler(unittest.TestCase):
         self.assertFalse(s.isAlive())
         self.assertFalse(s.stopped())
         self.assertTrue(s._newJobs.empty())
-        j = CountJob(1)
+        j = CountJob(1, 0, s)
         s.addJob(j, 0.0)
         self.assertFalse(s._newJobs.empty())
         s.start()
         self.assertFalse(s.stopped())
         self.assertTrue(s.isAlive())
-        time.sleep(0.01)
+        time.sleep(0.5)
         self.assertTrue(s._newJobs.empty())
+
+##del test_Scheduler
 
 if __name__ == '__main__':
     unittest.main(exit = False)
